78e4a6568875c72a37197634ac65c07f75a9251f
nkleinsasser2 <123771372+nkleinsasser2@users.noreply.github.com>
2024-01-04 21:58:35 -0600

    Updated Cluster Run Code

--- rna_draw/default_config.sh
+++ rna_draw/default_config.sh
@@ -8,27 +8,6 @@ module load anaconda
 conda activate py3
 cd {JOB_DIR}
 
-failure_count=0
-success_count=0
-
 for dbn_file in *.dbn; do
     python $WORK/rna_draw/rna_draw/run_rna_draw.py "$dbn_file"
-
-    if [ $? -eq 0 ]; then
-        ((success_count++))
-    else
-        ((failure_count++))
-    fi
-
-    total=$((success_count + failure_count))
-
-    if [ $total -ne 0 ]; then
-        percent=$((100 * success_count / total))
-    else
-        percent=0
-    fi
-
-    echo "Success percentage: $percent%"
-    echo "Total: $total"
-    echo "Success: $success_count"
 done
